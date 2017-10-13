47b9dd8940f6745df059755d2985c6a2b99b36c9
Henery Hawk <HeneryH@users.noreply.github.com>
2017-10-13 12:21:04 -0400

    Update rfid.js

--- userMetrics/rfid.js
+++ userMetrics/rfid.js
@@ -6,71 +6,54 @@ exports.timeoutOffset = require('../metrics.js').timeoutOffset;
 exports.futureDateOffset = require('../metrics.js').futureDateOffset;
 
 exports.metrics = {
-  V:
+  RFID_UID:
   {
-    name: 'V',
-    regexp: /\b(?:V?BAT|VOLTS|V)\:([\d\.]+)v?\b/i,
+    name: 'UID',
+    regexp: /UID\:([0-9a-fA-F]+)/i,
     value: '',
     duplicateInterval: 3600,
-    unit: 'v',
-    graph: 1,
-    graphOptions:
-    {
-      legendLbl: 'VOLTZ!',
-      lines:
-      {
-        fill: true,
-        lineWidth: 1
-      },
-      yaxis:
-      {
-        min: 0,
-        autoscaleMargin: 0.25
-      }
-    }
+    unit: '',
+//    graph: 1,
+//    graphOptions:
+//    {
+//      legendLbl: 'VOLTZ!',
+//      lines:
+//      {
+//        fill: true,
+//        lineWidth: 1
+//      },
+//      yaxis:
+//      {
+//        min: 0,
+//        autoscaleMargin: 0.25
+//      }
+//    }
   },
-  H:
-  {
-    name: 'H',
-    regexp: /\bH\:([\d\.]+)\b/i,
-    value: '',
-    duplicateInterval: 3600,
-    unit: '%',
-    pin: 1,
-    graph: 1,
-    graphOptions:
-    {
-      legendLbl: 'Humidity',
-      lines:
-      {
-        lineWidth: 1
-      },
-      yaxis:
-      {
-        autoscaleMargin: 0.25
-      },
-      series:
-      {
-        curvedLines:
-        {
-          active: true,
-          apply: true,
-          //monotonicFit: true, //makes the curve tight to datapoints
-          //nrSplinePoints: 5 //number of sample points (of the spline) in between two consecutive points (more = smoother)
-        }
-      }
-    }
-  }
 };
 
 //example of overriding an event
 exports.events = {
-  sprinklersOddDays : { label:'Odd days Z1-9 @3AM', icon:'clock', descr:'Run Zones 1-9, odd days @3AM', nextSchedule:function(node) { return exports.timeoutOffset(3,0); }, scheduledExecute:function(node) { if ((new Date().getDate()%2)==1) sendMessageToNode({nodeId:node._id, action:'PRG 1:400 2:400 3:400 4:300 5:400 6:400 7:400 8:300 9:150' /*runs stations 1-5 (300sec each))*/}); } },
-};
+  RFIDAlert : { label:'RFID : Card Detected', icon:'audio', descr:'Alert sound when RFID Card is detected', 
+                                         serverExecute:function(node) { 
+                                                if (node.metrics['UID'] && node.metrics['UID'].value == '5007de16' ) {
+                                                      io.sockets.emit('PLAYSOUND', 'sounds/access_granted.wav'); 
+                                                } else {
+                                                      io.sockets.emit('PLAYSOUND', 'sounds/access_denied.wav'); 
+                                               };                  <-- what is with this semicolon?  
+                                         } 
+                              },};
 
 //example of defining a property to use anywhere in the app/events or in other custom functions
 exports.ONEDAYHOURS = 24;
-
+                                               
+//example of defining a custom mote                                               
+exports.motes = {
+  RFIDMote: {
+      label  : 'RFID Sensor',
+      icon   : 'icon_sonar.png',   <-- create one
+      settings: { lowVoltageValue: '' }, //blank will make it inherit from global settings.json lowVoltageValue, a specific value overrides the general setting, user can always choose his own setting in the UI
+  },
+}
 //example of defining a general purpose function to use in the app/events or in other custom functions
 exports.secondsInOneDay = function() {
   var result = exports.ONEDAYHOURS * 3600;
